5e0e4f09359f4001dc12e99acf7705716bb2dace
Paweł <wisniewskipawe@gmail.com>
2016-08-17 03:59:32 +0200

    Ddane nowe strony

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
-    $('#search').click(function(){
-      alert('ok');
-    });
     
     $('a[href="#login"]').click(function(){
         $('body').load('login.html');
-                //.html('<h1>OK</h1>');
-        // $('container').load('login.html');
     });
+    
+    $('a[href="#profil"]').click(function(){
+        $('table').load('profil.html');
+    });
+    
+    $('a[href="#umowy"]').click(function(){
+        $('table').load('umowy.html');
+    });
+    $('a[href=#adress]').click(function(){
+        $('form').removeClass('hidden').addClass('show');
+    });
+    
+    $('a[href=#adress]').dblclick(function(){
+       $('form').remoweClass('show').addClass('hidden');
+    });
+    
+    $('a[href=#city]').click(function(){
+    $('#city').removeClass('hidden')
+    });
+  
+   $('a[href="#country"]').click(function(){
+       $('#country').removeClass('hidden');
+   });
+   
+   $('a[href="#voivodeship"]').click(function(){
+       $('#voivodeship').removeClass('hidden');
+   });
+   
+   $('a[href="#postcode"]').click(function(){
+       $('#postcode').removeClass('hidden');
+   })
+    
 });
